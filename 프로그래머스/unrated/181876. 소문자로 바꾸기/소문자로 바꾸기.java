21821a4b009cd910e35e73840f32e0097e88c1fb
callikys <134522414+callikys@users.noreply.github.com>
2023-12-22 14:27:32 +0900

    [level unrated] Title: 소문자로 바꾸기, Time: 9.36 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(int i = 0; i<myString.length(); i++) {
+            if(Character.isUpperCase(myString.charAt(i))) {
+                answer += Character.toLowerCase(myString.charAt(i));
+            } else answer += myString.charAt(i);
+        }
+        return answer;
+    }
+}
