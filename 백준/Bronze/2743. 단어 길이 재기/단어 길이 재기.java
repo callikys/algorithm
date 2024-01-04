59b9a33e0e90564436623f7a3298bd5be29ce936
callikys <134522414+callikys@users.noreply.github.com>
2024-01-05 00:57:10 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		char[] b = a.toCharArray();
+		
+		int i=0;
+		for(i=0; i<b.length; i++) {
+			
+		}
+		System.out.println(i);
+		
+	}
+
+}
