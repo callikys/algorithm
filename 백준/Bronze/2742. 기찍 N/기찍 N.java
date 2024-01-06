d233cd10adbc3a7bfb72cfcb8ef1e819c1d90e77
callikys <134522414+callikys@users.noreply.github.com>
2024-01-07 00:03:57 +0900

    [Bronze IV] Title: 기찍 N, Time: 1108 ms, Memory: 30964 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int i;
+		int N;
+		N = scanner.nextInt();
+		
+		for(i=N; i>0; i-- ) {
+			System.out.println(i);
+		}
+
+	}
+
+}
