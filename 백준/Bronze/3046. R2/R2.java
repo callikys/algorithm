422a136bced65b61a17f18ac175506ea5e8b8021
callikys <134522414+callikys@users.noreply.github.com>
2024-01-10 00:03:29 +0900

    [Bronze IV] Title: R2, Time: 204 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long r1 = sc.nextLong();
+        long s = sc.nextLong();
+
+        System.out.println(s*2 - r1);
+    }
+}
