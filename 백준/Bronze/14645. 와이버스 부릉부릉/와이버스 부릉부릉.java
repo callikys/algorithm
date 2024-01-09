5695a28d25b34ba88af5173c20b9a72e5b4a2273
callikys <134522414+callikys@users.noreply.github.com>
2024-01-09 21:53:47 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 196 ms, Memory: 17080 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        
+        for(int i = 0; i<N; i++) {
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+        }
+        System.out.println("비와이");
+    }
+}
