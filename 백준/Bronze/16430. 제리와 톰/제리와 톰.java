fd8c8dd2f80f5b629b26343f5a818723bf30f0f5
callikys <134522414+callikys@users.noreply.github.com>
2024-01-11 23:18:06 +0900

    [Bronze V] Title: 제리와 톰, Time: 204 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long A = sc.nextLong();
+        long B = sc.nextInt();
+        System.out.println(B-A);
+        System.out.println(B);
+    }
+}
