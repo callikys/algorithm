a9eb8d312670bbd3bc19bca732a58ef21c25b368
callikys <134522414+callikys@users.noreply.github.com>
2024-01-11 23:17:06 +0900

    [Bronze V] Title: 홍익대학교, Time: 208 ms, Memory: 17540 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long A = sc.nextLong();
+        System.out.println(A-1946);
+    }
+}
