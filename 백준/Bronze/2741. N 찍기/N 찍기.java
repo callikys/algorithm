1d3b1632486b685ab88e1e5ad84875f6e7bb2e95
callikys <134522414+callikys@users.noreply.github.com>
2024-01-31 20:51:49 +0900

    [Bronze V] Title: N 찍기, Time: 1112 ms, Memory: 33080 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N;
+		int i;
+		N = scanner.nextInt();
+		
+		for(i = 1; i<=N; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
