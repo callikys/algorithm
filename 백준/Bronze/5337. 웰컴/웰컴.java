ecc2e5dbe6712b9063062a801e4ea34b99c354b9
callikys <134522414+callikys@users.noreply.github.com>
2024-02-02 00:01:30 +0900

    [Bronze V] Title: 웰컴, Time: 108 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+
+}
