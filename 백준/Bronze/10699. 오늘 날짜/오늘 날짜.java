43bcc392ee0b78b8e9344270d1236a2674d0b6a1
callikys <134522414+callikys@users.noreply.github.com>
2024-02-04 00:00:45 +0900

    [Bronze V] Title: 오늘 날짜, Time: 192 ms, Memory: 17036 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.time.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDate date = LocalDate.now();
+		System.out.println(date);
+	}
+
+}
