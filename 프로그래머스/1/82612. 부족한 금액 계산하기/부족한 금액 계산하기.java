d064030c334a23c949cb0cc49463da16446d9d00
callikys <134522414+callikys@users.noreply.github.com>
2024-03-18 01:01:23 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.13 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        for(int i = 0; i<count; i++) {
+            answer += price * (i+1);
+        }
+        if(money > answer) {
+            answer = 0;
+        } else answer = answer - money;
+        return answer;
+    }
+}
