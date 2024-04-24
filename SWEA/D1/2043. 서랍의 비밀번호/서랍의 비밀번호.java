ff16c09668508bfaedb7411ad69c5e507f98f2e2
callikys <134522414+callikys@users.noreply.github.com>
2024-04-25 00:48:52 +0900

    [D1] Title: 서랍의 비밀번호, Time: 131 ms, Memory: 29,220 KB -BaekjoonHub

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int p, k;
+		p=sc.nextInt();
+        k=sc.nextInt();
+        System.out.println(p-k+1);
+
+	}
+}
